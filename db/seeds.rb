85399615a0f450daff812374fd5989074ff9be74
Oscar Welton <oscarwelton@gmail.com>
2023-02-28 19:47:44 +0200

    everything works - the ui needs work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  article = Article.create(
+    title:  Faker::Name.name,
+    content: Faker::Quote.famous_last_words
+  )
+end
